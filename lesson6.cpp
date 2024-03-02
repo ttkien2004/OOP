3857bff422635545d5c0c319fb9055abdf34179b
ttkien2004 <136047387+ttkien2004@users.noreply.github.com>
2024-03-02 14:32:04 +0700

    Create lesson6.cpp

---
+++ lesson6.cpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+class A{
+private:
+    int x, y;
+public:
+    //Default Contructor
+    A(){
+        x = 0;
+        y = 0;
+    }
+    //Copy Constructor
+    // A(const A& a){
+    //     this->x = a.x;
+    //     this->y = a.y;
+    // }
+    //Constructor that has parameter
+    A(int x, int y){
+        A::x = x; //this->x = x
+        A::y = y; //this->y = y 
+    }
+    friend ostream& operator<<(ostream& COUT, A& a); // friend function
+    friend void print(A); // friend global function
+    friend void change(A&);
+};
+
+class CHocSinh{
+private:
+    string id;
+public:
+    void Nhap(){
+        getline(cin,this->id);
+        cout << "ID has been typed in\n";
+    }
+    void Xuat(){
+        cout << id;
+    }
+};
+ostream& operator<<(ostream& COUT, A& a){
+    COUT << a.x << " " << a.y << endl;
+    return COUT;
+}
+void print(A a){
+    cout << a.x << " " << a.y << endl;
+}
+void change(A& a){
+    a.x = 0;
+    a.y = 0;
+}
+int main()
+{
+    A a; // Default Constructor will be called
+    A b(3,4); // Constructor that has parameter
+    A d = b; // Copy Constructor
+    cout << b;
+    cout << d;
+    
+    CHocSinh a1, b1;
+    a1.Nhap();
+    b1 = a1;
+    b1.Xuat();
+    return 0;
+}
